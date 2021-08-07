b17a732a6deba134ab4fe587efa6e146297c2105
gui230 <guilherme.goes.garci@outlook.com>
2021-08-07 15:06:05 -0300

    Deixando o código mais legivel

--- Cpf_Cnpj.py
+++ Cpf_Cnpj.py
@@ -2,6 +2,56 @@
 from validate_docbr import CPF,CNPJ
 
 
+class Documento:
+    @staticmethod
+    def cria_documento(documento):
+        if len(documento) == 11:
+            return DocCpf(documento)
+        elif len(documento) == 14:
+            return DocCnpj(documento)
+        else:
+            raise ValueError('Quantidade de dígitos esta incorreta!!')
+
+class DocCpf:
+    def __init__(self,documento):
+        if self.valida(documento):
+            self.cpf = documento
+        else:
+            raise ValueError('Cpf inválido !!')
+
+    def __str__(self):
+        return self.formata()
+
+    def valida(self, documento):
+        validador = CPF()
+        return validador.validate(documento)
+
+    def formata(self):
+        mascara = CPF()
+        return mascara.mask(self.cpf)
+
+class DocCnpj:
+    def __init__(self, documento):
+        if self.valida(documento):
+            self.cnpj = documento
+        else:
+            raise ValueError('Cnpj inválido')
+
+    def __str__(self):
+        return self.formata()
+
+    def valida(self,documento):
+        validate_cnpj = CNPJ()
+        return validate_cnpj.validate(documento)
+
+    def formata(self):
+        mascara = CNPJ()
+        return mascara.mask(self.cnpj)
+
+
+
+# Abaixo é um modelo de estudo que não esta muito legivel
+'''
 class Cpf_Cnpj:
     def __init__(self, documento,tipo_documento):
         self.tipo_documento = tipo_documento
@@ -56,4 +106,4 @@ class Cpf_Cnpj:
         if self.tipo_documento == 'cpf' or self.tipo_documento == 'CPF':
             return self.format_cpf()
         elif self.tipo_documento == 'cnpj' or self.tipo_documento == 'CNPJ':
-            return self.formata_cnpj()
+            return self.formata_cnpj() '''
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 "Usaremos este arquivo para testar as lógicas"
 from validate_docbr import CPF,CNPJ
-from Cpf_Cnpj import Cpf_Cnpj
+from Cpf_Cnpj import Documento
 
 exemplo_cnpj = "35379838000112"
 exemplo_cpf = "32007832062"
@@ -8,10 +8,10 @@ exemplo_cpf = "32007832062"
 #cnpj = CNPJ()
 #print(cnpj.validate(exemplo_cnpj))
 
-documento = Cpf_Cnpj(exemplo_cnpj,'cnpj')
+documento = Documento.cria_documento(exemplo_cnpj)
 print('')
 print(f'CNPJ: {documento}')
 
-documento2 = Cpf_Cnpj(exemplo_cpf,'cpf')
+documento2 = Documento.cria_documento(exemplo_cpf)
 print(f'CPF: {documento2}')
 
