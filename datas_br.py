6c91b03bb5ed243167770609a81187de2237f2c1
gui230 <guilherme.goes.garci@outlook.com>
2021-08-09 19:53:49 -0300

    Classe Data

---
+++ datas_br.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timedelta
+
+class DatasBr:
+    def __init__(self):
+        self.momento_cadastro = datetime.today()
+
+    def __str__(self):
+        return self.formata_data()
+
+    def mes_cadastro(self):
+        mes_do_ano = ['janeiro','fevereiro','março','abril',
+                      'maio','junho','julho','agosto','setembro','outubro','novembro','dezembro']
+        mes_cadastro = self.momento_cadastro.month
+
+        return mes_do_ano[mes_cadastro-1]
+
+    def dia_semana(self):
+        dia_semana = ['segunda','terça','quarta','quinta','sexta','sabado','domingo']
+        dia_cadastro = self.momento_cadastro.weekday()
+
+        return dia_semana[dia_cadastro]
+
+    def formata_data(self):
+        data_formatada = self.momento_cadastro.strftime('%d/%m/%Y %H:%M')
+        return data_formatada
